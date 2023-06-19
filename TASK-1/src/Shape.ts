5da6f560fa34fa2993685a5ee79a832448428a78
Pavel Shpakovich <pavel.shpakovich@inspirebrands.com>
2023-06-19 16:22:14 +0300

    [task1]

--- TASK-1/src/Shape.ts
+++ TASK-1/src/Shape.ts
@@ -1,3 +1,32 @@
+import { Point } from "./point";
+
 export abstract class Shape {
+  protected color: string;
+  protected filled: boolean;
+  protected points: Point[];
+
   abstract getType(): string;
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string, filled: boolean);
+  constructor(points: Point[], color?: string, filled?: boolean) {
+    if (points.length < 3) {
+      throw new Error("Provide at least 3 points");
+    }
+
+    this.points = points;
+    this.color = color ?? "green";
+    this.filled = filled ?? true;
+  }
+
+  toString(): string {
+    const stringifiedPoints = this.points.map((point) => point.toString()).join(", ");
+    const filled = this.filled ? "filled" : "not filled";
+
+    return `A Shape with color of ${this.color} and ${filled}. Points: ${stringifiedPoints}.`;
+  }
+
+  getPerimeter(): number {
+    return this.points.reduce((prev, curr, index, arr) => prev + curr.distance(arr[index + 1] || arr[0]), 0);
+  }
 }
